c8fcbfdec11d238e8733cf0b6e7da9a32200b89e
FERNANDO\Fernando <fernando.htcordeiro@gmail.com>
2018-10-23 16:25:30 -0200

    Atualização do app

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from '../../providers/moovie/moovie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,18 +13,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MoovieProvider
+  ]
 })
 export class FeedPage {
-public nome_usuario:string = "Fernando Cordeiro o melhor";
+  public objeto_feed={
+    titulo:"Fernando Cordeiro",
+    data:"November 5, 1955",
+    descricao:"Criando um super app incrivel.",
+    qntd_likes: 12,
+    qntd_commments: 4,
+    time_comments:"11h ago"
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public lista_filmes = new Array<any>();
+
+public nome_usuario:string = "Fernando Cordeiro ";
+ 
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private movieProvider: MoovieProvider
+  ) {
   }
 
 public somaDoisNumeros(num1:number, num2:number): void{
-  alert(num1 + num2);
+  //alert(num1 + num2);
 }
   ionViewDidLoad() {
-    //this.somaDoisNumeros(10, 99);
+      this.movieProvider.getLatestMoovie().subscribe(
+        data=>{
+          const response = (data as any);
+          const objeto_retorno = JSON.parse(response._body);
+          this.lista_filmes = objeto_retorno.results;
+          
+          console.log(objeto_retorno);
+        }, error => {
+          console.log(error);
+        }
+        
+      )
   }
 
 }
